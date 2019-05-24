ef9d6a7fd98497e96c3e5f89d7175912dbbe7c3e
Andrés Cruz <arcruz@uc.cl>
2019-05-24 00:13:07 -0400

    Fix the order of columns in trust_join

--- R/trust_join.R
+++ R/trust_join.R
@@ -3,68 +3,56 @@
 #' Performs a fuzzy join that trusts the algorithm. Allows to provide custom matches.
 #'
 #' @export
-trust_join <- function(x, y, by, max_dist = Inf,
-                       method = c("osa", "lv", "dl", "hamming", "lcs", "qgram",
-                                  "cosine", "jaccard", "jw", "soundex"),
-                       mode = "inner",
-                       custom_match = NULL,
-                       ignore_case = FALSE,
-                       match_cols = FALSE,
-                       ...) {
+trust_join <- function (x, y, by, max_dist = Inf, method = c("osa", "lv", "dl", 
+                                                             "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex"), 
+                        mode = "inner", custom_match = NULL, ignore_case = FALSE, 
+                        match_cols = FALSE, ...) {
+  # generate symbols from argument values:
+  
   var_by <- enquo(by)
   var_by_x <- sym(str_c(by, ".x"))
   var_by_y <- sym(str_c(by, ".y"))
   var_match <- sym(str_c(".", by, "_match"))
-
+  
   v_order <- unique(x[[quo_name(var_by)]])
-
-  res <- stringdist_join(x = x, y = y, by = by, max_dist = max_dist, method = method,
-                         mode = mode, ignore_case = ignore_case,
-                         distance_col = ".dist",
-                         ... = ...)
-
-  if (!is.null(custom_match)){
-
-    df_custom_match <- data.frame(
-      value_x = names(custom_match),
-      value_y = custom_match
-    )
-
-    df_custom_match <- df_custom_match %>%
-      mutate(.dist2 = -1)
-
-    names(df_custom_match) <- c(quo_name(var_by_x), quo_name(var_by_y),
+  
+  res <- stringdist_join(x = x, y = y, by = by, max_dist = max_dist, 
+                         method = method, mode = mode, ignore_case = ignore_case, 
+                         distance_col = ".dist", ... = ...)
+  if (!is.null(custom_match)) {
+    df_custom_match <- data.frame(value_x = names(custom_match), 
+                                  value_y = custom_match)
+    df_custom_match <- df_custom_match %>% mutate(.dist2 = -1)
+    names(df_custom_match) <- c(quo_name(var_by_x), quo_name(var_by_y), 
                                 ".dist2")
-
-    suppressMessages({suppressWarnings({res <- res %>% left_join(df_custom_match)})})
-
-    res <- res %>%
-      replace_na(list(.dist2 = Inf)) %>%
-      rowwise() %>%
-      mutate(.dist = min(.dist, .dist2)) %>%
-      ungroup() %>%
+    suppressMessages({
+      suppressWarnings({
+        res <- res %>% left_join(df_custom_match)
+      })
+    })
+    res <- res %>% replace_na(list(.dist2 = Inf)) %>% rowwise() %>% 
+      mutate(.dist = min(.dist, .dist2)) %>% ungroup() %>% 
       select(-.dist2)
   }
+  res <- res %>% group_by(!!var_by_x) %>% arrange(.dist) %>% 
+    slice(1) %>% ungroup() %>% slice(match(v_order, .[[quo_name(var_by_x)]]))
 
-  res <- res %>%
-    group_by(!!var_by_x) %>%
-    arrange(.dist) %>%
-    slice(1) %>%
-    ungroup() %>%
-    slice(match(v_order, .[[quo_name(var_by_x)]]))
+  # names(res)[1] <- quo_name(var_by)
+  names(res)[which(names(res) == quo_name(var_by_x))] <- quo_name(var_by)
 
-  names(res)[1] <- quo_name(var_by)
   res[[quo_name(var_match)]] <- res[[quo_name(var_by_y)]]
-
-  res[[quo_name(var_by_x)]] <- NULL; res[[quo_name(var_by_y)]] <- NULL
-
+  
+  # res[[quo_name(var_by_x)]] <- NULL
+  res[[quo_name(var_by_y)]] <- NULL
   res <- select(res, !!var_by, !!var_match, ".dist", everything())
-
   if (match_cols == F) {
     res[[quo_name(var_match)]] <- NULL
     res$.dist <- NULL
   }
-
+  
+  # set column order appropiately:
+  res <- res %>% 
+    select(unique(c(colnames(x), colnames(y))))
+  
   return(res)
-
 }
