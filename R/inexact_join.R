9ed1ce1d3db831499e0890eba127c06e9b41704c
Andrés <arcruz@uc.cl>
2020-01-07 12:43:46 -0300

    change join function to data.table

--- R/inexact_join.R
+++ R/inexact_join.R
@@ -3,113 +3,88 @@
 #' The function allows to provide custom matches.
 #'
 #' @importFrom dplyr "%>%"
+#' @importFrom data.table "%chin%"
 #'
 #' @export
 
-inexact_join <- function(x, y, by, max_dist = Inf, method = c("osa", "lv", "dl", 
-                                                            "hamming", "lcs", 
-                                                            "qgram", "cosine", 
-                                                            "jaccard", "jw", 
-                                                            "soundex"), 
-                       mode = "inner", custom_match = NULL, ignore_case = FALSE, 
-                       match_cols = FALSE, ...) {
+inexact_join <- function(x, y, by, max_dist = Inf, 
+                         method = c("osa", "lv", "dl", "hamming", "lcs", 
+                                    "qgram", "cosine", "jaccard", "jw", 
+                                    "soundex"), 
+                         mode = "left", custom_match = NULL, ignore_case = FALSE, 
+                         match_cols = FALSE, output = "data.frame",
+                         ...) {
+  dt_x <- data.table::as.data.table(x); dt_y <- data.table::as.data.table(y)
   
-  # generate symbols from argument values:
-  var_by <- rlang::enquo(by)
-  var_by_x <- rlang::sym(stringr::str_c(by, ".x"))
-  var_by_y <- rlang::sym(stringr::str_c(by, ".y"))
-  var_match <- rlang::sym(stringr::str_c(".", by, "_match"))
+  v_x <- unique(dt_x[, get(by)]); v_y <- unique(dt_y[, get(by)])
   
-  v_order <- unique(x[[rlang::quo_name(var_by)]])
+  st <- stringdist::stringdistmatrix(a = v_x, b = v_y, useNames = T, ...)
   
-  res <- fuzzyjoin::stringdist_join(
-    x = x, y = y, by = by, max_dist = max_dist, 
-    method = method, mode = mode, ignore_case = ignore_case, 
-    distance_col = ".dist", ... = ...)
+  f_min_matrix <- function(x){colnames(st)[which(x == min(x) & x <= max_dist)[1]]}
   
-  if (!is.null(custom_match)) {
-    
-    if (class(v_order) == "character"){
-      right_na <- NA_character_
-    } else if (class(v_order) == "numeric"){
-      right_na <- NA_real_
-    } else {
-      right_na <- NA
-    }
-    
-    custom_match <- tidyr::replace_na(custom_match, right_na)
-    
-    x_values_to_missing <- names(custom_match[is.na(custom_match)])
-    
-    df_custom_match <- data.frame(value_x = names(custom_match), 
-                                  value_y = custom_match)
-    df_custom_match <- df_custom_match %>% dplyr::mutate(.dist2 = -1)
-    names(df_custom_match) <- c(rlang::quo_name(var_by_x), 
-                                rlang::quo_name(var_by_y), 
-                                ".dist2")
-
-    suppressMessages({
-      suppressWarnings({
-        res <- res %>% dplyr::left_join(df_custom_match)
-      })
-    })
-    
-    res <- res %>% 
-      tidyr::replace_na(list(.dist2 = Inf)) %>% 
-      dplyr::rowwise() %>% 
-      dplyr::mutate(.dist = min(.dist, .dist2)) %>% 
-      dplyr::ungroup() %>% 
-      dplyr::select(-.dist2)
-  }
-  
-  res <- res %>% 
-    dplyr::group_by(!!var_by_x) %>% 
-    dplyr::arrange(.dist) %>% 
-    dplyr::slice(1) %>% 
-    dplyr::ungroup() %>% 
-    dplyr::slice(match(v_order, .[[rlang::quo_name(var_by_x)]]))
+  dt_st <- data.table::data.table(
+    orig = v_x, 
+    rep  = as.vector(apply(st, 1, f_min_matrix)),
+    .dist = apply(st, 1, min)
+  )
   
   if (!is.null(custom_match)){
-    if (length(x_values_to_missing) > 0){
-      res_to_missing <- dplyr::filter(res, !!var_by_x %in% x_values_to_missing) %>% 
-        dplyr::mutate_at(dplyr::vars(pais.y:.dist), 
-                         function(x){
-                           type <- typeof(x)
-                           switch(
-                             type,
-                             'double' = NA_real_,
-                             'integer' = NA_integer_,
-                             'character' = NA_character_,
-                             'complex' = NA_complex_
-                           )
-                         })
-      
-      res_non_missing <- dplyr::filter(res, !(!!var_by_x %in% x_values_to_missing))
-      
-      res <- dplyr::bind_rows(res_to_missing, res_non_missing) %>% 
-        dplyr::slice(match(v_order, .[[rlang::quo_name(var_by_x)]]))
-    }
+    dt_custom_match <- data.table::data.table(
+      orig = names(custom_match),
+      rep  = custom_match,
+      .dist = -1
+    )
+    
+    st_long <- data.table::melt(data.table::as.data.table(st, keep.rownames = T), 
+                                id.vars = "rn")
+    
+    data.table::setnames(st_long, c("orig", "rep", ".dist"))
+    
+    st_long[, .dist := .dist * -1]
+    
+    dt_custom_match[.dist == -1] <- data.table::merge.data.table(
+      x = dt_custom_match[.dist == -1, !c(".dist")], 
+      y = st_long,
+      by = c("orig", "rep"),
+      all.x = T, sort = F
+    )
+    
+    # don't forget to add: https://stackoverflow.com/a/27979637
+    dt_st[orig %chin% dt_custom_match[, orig],] <- dt_custom_match
   }
   
-  names(res)[which(names(res) == rlang::quo_name(var_by_x))] <- rlang::quo_name(var_by)
-
-  res[[rlang::quo_name(var_match)]] <- res[[rlang::quo_name(var_by_y)]]
+  dt_st <- data.table::merge.data.table(
+    dt_st, data.table:::unique.data.table(dt_y, by = by),
+    by.x = "rep", by.y = by,
+    all.x = T, sort = F
+  )
   
-  res[[rlang::quo_name(var_by_y)]] <- NULL
+  ret <- data.table::merge.data.table(dt_x, dt_st, 
+                                      by.x = by, by.y = "orig",
+                                      all.x = T, sort = F)
   
-  res <- dplyr::select(res, !!var_by, !!var_match, ".dist", dplyr::everything())
+  data.table::setnames(ret, old = "rep", new = ".match")
   
-  # set column order appropiately:
-  res <- res %>% dplyr::select(!!var_by,
-                               setdiff(unique(colnames(x)), !!var_by),
-                               !!var_match, .dist,
-                               setdiff(unique(colnames(y)), !!var_by)
-                               )
+  data.table::setcolorder(ret, c(names(x), setdiff(names(y), names(x))))
   
-  if (match_cols == F) {
-    res[[rlang::quo_name(var_match)]] <- NULL
-    res$.dist <- NULL
+  ret[, .custom_match := data.table::fifelse(
+    .dist < 0, T, F
+  )]
+  
+  ret[, .dist := abs(.dist)]
+  
+  if (match_cols == F){
+    ret <- ret[, !c(".match", ".dist", ".custom_match")]
   }
   
-  return(res)
+  if (output == "data.frame") {
+    return(as.data.table(ret[]))
+  } else if (output == "data.table"){
+    return(ret[])
+  } else if (output == "tibble"){
+    return(tibble::as_tibble(ret[]))
+  } else {
+    stop("Invalid 'output' argument. Valid options are 'data.frame' (default), 'data.table' and 'tibble'.")
+  }
+  
 }
